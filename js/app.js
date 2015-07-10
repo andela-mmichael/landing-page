715519cfe3a768c18994e9d5e3245408ab3c9ad0
mel <mel.michael@andela.co>
2015-07-10 15:35:38 +0100

    Implement routing and local storage

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-angular.module('LandingPageApp', ['ngMaterial'])
-  .config(function($mdThemingProvider) {
+
+var app = angular.module('LandingPageApp', [
+  'ngMaterial',
+  'ui.router'
+  ]);
+
+var storageKey = 'CMS';
+
+app.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
       .primaryPalette('blue-grey')
       .accentPalette('amber');
-})
+});
 
-  .controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', function($scope) {
 
     $scope.product = {
       caption: "We have all the Best Stories",
@@ -15,4 +22,45 @@ angular.module('LandingPageApp', ['ngMaterial'])
       image: 'http://srobbin.com/wp-content/uploads/2012/05/books.jpg'
     };
 
+    try {
+      $scope.product = JSON.parse(localStorage.getItem(storageKey));
+      $scope.product.image = 'http://srobbin.com/wp-content/uploads/2012/05/books.jpg';
+    } catch(e) {
+      console.log(e);
+    }
+    
   }]);
+
+app.controller('AdminController', ['$scope', function($scope) {
+
+  try {
+    $scope.data = JSON.parse(localStorage.getItem(storageKey));
+  } catch(e) {
+    console.log(e);
+  }
+
+  $scope.submitForm = function(data){
+    data.created_at = new Date().getTime();
+    localStorage.setItem(storageKey, JSON.stringify(data));
+  };
+
+}]);
+
+
+ app.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "views/index.html",
+      controller: 'MainController'
+    })
+    .state('admin', {
+      url: "/admin",
+      templateUrl: "views/admin.html",
+      controller: 'AdminController'
+    });
+
+ });
