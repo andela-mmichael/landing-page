24681f820efc30ac395c35d4f28abab3daadd70e
mel <mel.michael@andela.co>
2015-07-10 14:39:30 +0100

    Add controller and linked it to view

--- js/app.js
+++ js/app.js
@@ -3,4 +3,16 @@ angular.module('LandingPageApp', ['ngMaterial'])
     $mdThemingProvider.theme('default')
       .primaryPalette('blue-grey')
       .accentPalette('amber');
-});
+})
+
+  .controller('MainController', ['$scope', function($scope) {
+
+    $scope.product = {
+      caption: "We have all the Best Stories",
+      name: "Best Story Books",
+      description: "So, what is the secret of successful description.",
+      pubDate: new Date(),
+      image: 'http://srobbin.com/wp-content/uploads/2012/05/books.jpg'
+    };
+
+  }]);
